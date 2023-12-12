a50b4072593f62ced4c33ee823de3091eb92d1ec
severeduck <92829782+severeduck@users.noreply.github.com>
2023-12-12 13:40:21 +0100

    parser

--- src/parser.rs
+++ src/parser.rs
@@ -26,10 +26,130 @@ impl Parser {
         // Implement parsing logic here based on your language's grammar
     }
 
-    // Add more parsing methods here, e.g., parse_expression, parse_statement, etc.
+    fn parse_lambda_expression(&mut self) -> Result<AstNode, String> {
+        self.expect_token(TokenType::Pipe)?; // Assuming '|' is used to denote the start of lambda parameters
+        let parameters = self.parse_parameters()?; // You will need to implement this method
+        self.expect_token(TokenType::Pipe)?; // Assuming '|' is used to denote the end of lambda parameters
+        let body = self.parse_expression()?; // You will need to implement this method
+        Ok(AstNode::LambdaExpression { parameters, body }) // Return a new AST node for the lambda
+    }
+
+    // Helper method to expect and consume a specific token type, returning an error if it's not found
+    fn expect_token(&mut self, token_type: TokenType) -> Result<(), String> {
+        match self.current_token {
+            Some(ref token) if token.type == token_type => {
+                self.next_token(); // Consume the token
+                Ok(())
+            },
+            _ => Err(format!("Expected token: {:?}", token_type)),
+        }
+    }
+
+    fn parse_parameters(&mut self) -> Result<Vec<Parameter>, String> {
+        let mut parameters = Vec::new();
+        while let Some(token) = &self.current_token {
+            match token.type {
+                TokenType::Identifier => {
+                    let param_name = token.value.clone();
+                    self.next_token(); // Consume the identifier
+                    let type_annotation = if matches!(self.current_token, Some(Token { type: TokenType::Colon, .. })) {
+                        self.next_token(); // Consume the colon
+                        Some(self.parse_type()?) // Parse the type
+                    } else {
+                        None
+                    };
+                    parameters.push(Parameter { name: param_name, type_annotation });
+                    if matches!(self.current_token, Some(Token { type: TokenType::Comma, .. })) {
+                        self.next_token(); // Consume the comma and continue
+                    } else {
+                        break; // End of parameters
+                    }
+                },
+                _ => return Err(format!("Expected parameter, found {:?}", token.type)),
+            }
+        }
+        Ok(parameters)
+    }
+
+    fn parse_expression(&mut self) -> Result<AstNode, String> {
+        let mut lhs = self.parse_primary()?;
+        while let Some(token) = &self.current_token {
+            match token.type {
+                TokenType::Plus | TokenType::Minus => {
+                    let operator = token.type;
+                    self.next_token(); // Consume the operator
+                    let rhs = self.parse_primary()?;
+                    lhs = AstNode::BinaryOperation {
+                        lhs: Box::new(lhs),
+                        operator,
+                        rhs: Box::new(rhs),
+                    };
+                },
+                _ => break,
+            }
+        }
+        Ok(lhs)
+    }
+
+    fn parse_primary(&mut self) -> Result<AstNode, String> {
+        match &self.current_token {
+            Some(Token { type: TokenType::Number, value }) => {
+                self.next_token(); // Consume the number token
+                Ok(AstNode::NumberLiteral(value.parse::<f64>().unwrap()))
+            },
+            Some(Token { type: TokenType::Identifier, value }) => {
+                self.next_token(); // Consume the identifier token
+                Ok(AstNode::Variable(value.clone()))
+            },
+            // Add more cases here as needed (e.g., parentheses, function calls)
+            _ => Err("Unrecognized primary expression".to_string()),
+        }
+    }
+
+    fn parse_type(&mut self) -> Result<Type, String> {
+        // Parsing type annotations
+        // For example, parsing a type identifier like 'int', 'string', etc.
+        match &self.current_token {
+            Some(Token { type: TokenType::Identifier, value }) => {
+                // Map the value to a Type enum variant
+                let type_enum = match value.as_str() {
+                    "int" => Type::Int,
+                    "string" => Type::String,
+                    // ... other type mappings ...
+                    _ => return Err(format!("Unknown type: {}", value)),
+                };
+                self.next_token(); // Consume the type token
+                Ok(type_enum)
+            }
+            _ => Err("Expected a type".to_string()),
+        }
+    }
 }
 
-// Define your AstNode struct and other related structures here
+// Define the AstNode variants for lambda expressions
+enum AstNode {
+    // ... existing variants ...
+    LambdaExpression {
+        parameters: Vec<Parameter>,
+        body: Box<AstNode>,
+    },
+    // ... other variants ...
+}
+
+// Assume a `Parameter` struct is defined elsewhere
+struct Parameter {
+    name: String,
+    type_annotation: Option<Type>,
+    // ... any additional fields ...
+}
+
+// Assume a `Type` enum is defined elsewhere to represent types
+enum Type {
+    // ... type variants ...
+}
+
+
+
 
 fn main() {
     // Example usage of the parser
