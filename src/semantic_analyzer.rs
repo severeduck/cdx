e2214e28556d0c267778fc9ae655e044f2b031f9
severeduck <92829782+severeduck@users.noreply.github.com>
2023-12-12 14:02:32 +0100

    semantic_analyzer

--- src/semantic_analyzer.rs
+++ src/semantic_analyzer.rs
@@ -1,16 +1,35 @@
+use crate::ast::{AstNode};
+use crate::lexer::{Token, TokenType};
+use crate::error::{YourErrorType};
 
-// Handles semantic analysis
+
 pub struct SemanticAnalyzer {
-    // Fields for semantic analysis
+    // You can add fields here if needed for context or state
 }
 
 impl SemanticAnalyzer {
     pub fn new() -> Self {
-        // Initialize the semantic analyzer
+        SemanticAnalyzer {
+            // Initialize any fields here
+        }
     }
 
     pub fn analyze(&self, ast: &AstNode) -> Result<(), String> {
-        // Perform semantic analysis on the AST
-        Ok(())
+        // Starting point for AST traversal and analysis
+        match ast {
+            AstNode::NumberLiteral(value) => {
+                // Check if the value is an integer
+                if value.fract() == 0.0 {
+                    Ok(())
+                } else {
+                    Err(format!("Error: Non-integer number found at line {}", ast.line_number))
+                }
+            },
+            // Add more matches for different AST node types
+            // ...
+            _ => Ok(()), // Default case if no specific handling is required
+        }
     }
+
+    // You can add more helper methods here for specific checks or AST node types
 }
